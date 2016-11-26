595ef078a77b1d8495d409fd9a50f91b4dffa458
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-11-26 18:05:47 +0100

    Add Enum classes

---
+++ src/main/java/com/backbase/mancala/dto/GameStatus.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.backbase.mancala.dto;
+
+/**
+ * @author ashraf
+ *
+ */
+public enum GameStatus {
+	
+	P1_Turn("Player 1's Turn!"),  
+	P2_Turn("Player 2's Turn!"),
+	GAME_OVER("Game is Over!");
+
+    private final String gameStatus;
+
+    private GameStatus(String gameStatus) {
+        this.gameStatus = gameStatus;
+    }
+
+	public String getGameStatus() {
+		return gameStatus;
+	}
+
+}
---
+++ src/main/java/com/backbase/mancala/dto/Winner.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.backbase.mancala.dto;
+
+/**
+ * @author ashraf
+ *
+ */
+public enum Winner {
+	
+	P1("Player 1 won!"),  
+	P2("Player 2 won!"),
+	TIE("It is a tie!");
+
+    private final String winner;
+
+    private Winner(String winner) {
+        this.winner = winner;
+    }
+
+	public String getWinner() {
+		return winner;
+	}
+
+}
